54ccfa146788214e8dcf32d1715eecc55a723a65
Emanuel Valdiviezo <emanuel.vald@gmail.com>
2021-02-01 03:59:49 -0300

    Se agrega Router para manejar rutas en la aplicación.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import DatosEmpleadoForm from "./components/datosEmpleado/DatosEmpleadoForm";
+import TestUno from "./components/testUno/TestUno";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 function App() {
   return (
-    <DatosEmpleadoForm />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={DatosEmpleadoForm} />
+          <Route exaxct path="/testuno" component={TestUno} />
+        </Switch>
+      </Router>
   );
 }
 
---
+++ src/components/testUno/TestUno.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TestUno = () => {
+    return (
+        <h1>TestUno</h1>
+    )
+}
+
+export default TestUno;
